98a9eeecfe4a714dbbc5b3462c07e96348eed672
Daniyar <daniyar>
2023-09-09 11:53:51 +0500

    Homework2

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Введите первое число");
+int a = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите второе число");
+int b = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите третье число");
+int c = Convert.ToInt32(Console.ReadLine());
+int max = a;
+if (max < b) max = b;
+if (max < c) max = c;
+
+System.Console.WriteLine( "Максимальное из этих чисел: " + max);
